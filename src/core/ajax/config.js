6b5539ba398ca09f574cd1be057bfb2d95f85e28
wuyaoxing <82565097@qq.com>
2017-08-09 15:24:27 +0800

    core

--- src/core/ajax/config.js
+++ src/core/ajax/config.js
@@ -1,17 +1,17 @@
-// import { SYS_CONF, U_TAG, PRELOAD } from 'appCfg'
+import { SYS_CONF, U_TAG, PRELOAD } from 'appCfg'
 
-// export const XHR_HEADER = {
-//     'X-SID': SYS_CONF['X-SID'],
-//     'X-PHPSESSID': SYS_CONF['X-PHPSESSID'],
-//     'X-OU-ID': `${PRELOAD && PRELOAD.user ? PRELOAD.user.ouId : ''}`,
-//     'X-Requested-With': 'XMLHttpRequest',
-//     _tag: U_TAG
-// }
+export const XHR_HEADER = {
+    'X-SID': SYS_CONF['X-SID'],
+    'X-PHPSESSID': SYS_CONF['X-PHPSESSID'],
+    'X-OU-ID': `${PRELOAD && PRELOAD.user ? PRELOAD.user.ouId : ''}`,
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-TAG': U_TAG
+}
 
-// export const updateHeader = ({ SID, PHPSESSID }) => {
-//     XHR_HEADER['X-SID'] = SID
-//     XHR_HEADER['X-PHPSESSID'] = PHPSESSID
-// }
+export const updateHeader = ({ SID, PHPSESSID }) => {
+    XHR_HEADER['X-SID'] = SID
+    XHR_HEADER['X-PHPSESSID'] = PHPSESSID
+}
 
 export const ROOT_PATH = '/api'
 export const CONTENT_TYPE = 'application/json'
@@ -22,4 +22,8 @@ export const errCodes = [
 ]
 
 // 存放向下放行 reject 的请求，供视图独立处理 404
-export const errWhiteList = []
+export const errWhiteList = [
+    '/projects/:id/members',    // 获取项目成员
+    '/tasks/:id/projectId',     // 获取任务相关的项目ID
+    '/projects/:prjId/tasks/:id',     // 获取任务数据
+]
--- src/core/ajax/index.js
+++ src/core/ajax/index.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 import HTTP_HANDLE from './handler'
-import { CONTENT_TYPE, ROOT_PATH } from './config'
-// import { REQ_INTERCEPTOR, RES_INTERCEPTOR } from './interceptors'
+import { CONTENT_TYPE, ROOT_PATH, XHR_HEADER } from './config'
+import { REQ_INTERCEPTOR, RES_INTERCEPTOR } from './interceptors'
 
 axios.defaults.headers.post['Content-Type'] = CONTENT_TYPE
 axios.defaults.headers.put['Content-Type'] = CONTENT_TYPE
 axios.defaults.baseURL = ROOT_PATH
-axios.defaults.headers.accpet = CONTENT_TYPE
+axios.defaults.headers.Accept = CONTENT_TYPE
 axios.defaults.responseType = 'json'
-// Object.assign(axios.defaults.headers, XHR_HEADER)
+Object.assign(axios.defaults.headers, XHR_HEADER)
 
-// axios.interceptors.request.use(REQ_INTERCEPTOR)
-// axios.interceptors.response.use(RES_INTERCEPTOR, error => Promise.reject(error))
+axios.interceptors.request.use(REQ_INTERCEPTOR)
+axios.interceptors.response.use(RES_INTERCEPTOR, error => Promise.reject(error))
 
 /**
  * 格式化参数
@@ -28,6 +28,11 @@ axios.defaults.responseType = 'json'
 //     // return urlParams
 // }
 
+const fixUrl = url => url.replace(/(.+)\/$/, '$1')
+const urlPattern = (url, arg) => ({
+    url: url.replace(/:[a-zA-Z]+/g, () => arg.shift()),
+    params: arg.length ? arg[0] : null
+})
 /**
  * http GET
  * @param {*} url String
@@ -62,18 +67,78 @@ export const DELETE = (url, params) => HTTP_HANDLE(axios.delete(url, params ? {
 
 /**
  * REQUEST
- * @param {*} url
- * @param {*} option
+ * @param {*} path
+ * @param {*} args
  */
-export const REQUEST = (url, option) => HTTP_HANDLE(axios(url, option))
+export const REQUEST = (path, ...args) => {
+    const {
+        url,
+        params
+    } = urlPattern(path, args)
+    return HTTP_HANDLE(axios(url, params))
+}
 
-export const PATH = (url, ...arg) => {
-    const reqUrl = url.replace(/:[a-zA-Z]+/g, () => arg.shift())
-    return {
-        get: params => GET(reqUrl, params),
-        post: params => POST(reqUrl, params),
-        delete: params => DELETE(reqUrl, params),
-        put: params => PUT(reqUrl, params),
-        request: params => REQUEST(reqUrl, params),
+/**
+ * 对常用 RESTful 请求格式的封装，支持多级资源请求
+ * 以一个实体 User 为例，基础 URL 为 `/api/user`，则：
+ * 查询 GET /api/user/:id
+ * 添加 POST /api/user
+ * 修改 PUT /api/user/:id
+ * 删除 DELETE /api/user/:id
+ *
+ * @export
+ * @class Api
+ */
+export default class Api {
+    constructor(url) {
+        this.url = fixUrl(url)
+    }
+
+    add(...m) {
+        const {
+            url,
+            params
+        } = urlPattern(this.url, m)
+        return POST(url, params)
+    }
+
+    update(...args) {
+        const {
+            url,
+            params
+        } = urlPattern(`${this.url}/:id`, args)
+        return PUT(url, params)
+    }
+
+    delete(...args) {
+        const {
+            url,
+            params
+        } = urlPattern(`${this.url}/:id`, args)
+        return DELETE(url, params)
+    }
+
+    one(...args) {
+        const {
+            url,
+            params
+        } = urlPattern(`${this.url}/:id`, args)
+        return GET(url, params)
+    }
+
+    all(...args) {
+        const {
+            url,
+            params
+        } = urlPattern(this.url, args)
+        return GET(url, params)
+    }
+
+    request(client, path, ...args) {
+        const {
+            url,
+            params
+        } = urlPattern(`${this.url}/:id${path}`, args)
+        return client(url, params)
     }
 }
